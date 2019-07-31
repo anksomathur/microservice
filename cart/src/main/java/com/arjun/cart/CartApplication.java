5dffaf3efce7fd73f8890bd44dfcf172c2fc6251
amathur <ankso.mathurs@gmail.com>
2019-07-31 15:28:15 +0530

    FIRST COMMIT

---
+++ cart/src/main/java/com/arjun/cart/CartApplication.java
@@ -0,0 +1,15 @@
+package com.arjun.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApplication.class, args);
+	}
+}
